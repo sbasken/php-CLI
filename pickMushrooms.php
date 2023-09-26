a0cc8a531cb7fc2ccea543420eec13fd84f8f011
sbasken <120995506+sbasken@users.noreply.github.com>
2023-09-26 10:53:14 -0700

    Create pickMushrooms.php

---
+++ pickMushrooms.php
@@ -0,0 +1,10 @@
+<?php
+  
+function pickMushrooms(){
+	global $location, $has_mushrooms;
+  if ($location !== "woods") {
+    echo "There aren't any mushrooms to pick!\n";
+  } else {
+    echo "You've picked some mushrooms.\n";
+    $has_mushrooms = TRUE;
+}
